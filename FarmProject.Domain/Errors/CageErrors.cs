ca8e48ff30270f8d3c8e1aaf4c883b958d5f7fb6
MaxNoragami <maxcraftmd@gmail.com>
2025-06-03 13:27:59 +0300

    feat: add base implementation of the Cage

---
+++ FarmProject.Domain/Errors/CageErrors.cs
@@ -0,0 +1,21 @@
+﻿using FarmProject.Domain.Common;
+
+namespace FarmProject.Domain.Errors;
+
+public class CageErrors
+{
+    public static readonly Error NoFemaleRabbit = new(
+        "Cage.NoFemaleRabbit", "No female breeding rabbit is assigned to this cage");
+
+    public static readonly Error NoMaleRabbit = new(
+        "Cage.NoMaleRabbit", "No male breeding rabbit is assigned to this cage");
+
+    public static readonly Error InvalidAssignment = new(
+        "Cage.InvalidAssignment", "No null reference of a breeding rabbit can be assigned to a cage");
+
+    public static readonly Error NegativeOffspringNum = new(
+        "Cage.NegativeOffspringNum", "Cannot add or remove negative num of offsprings");
+
+    public static readonly Error OverOffspringNum = new(
+        "Cage.OverOffspringNum", "Cannot remove more offsprings than available");
+}
---
+++ FarmProject.Domain/Models/Cage.cs
@@ -0,0 +1,74 @@
+﻿using FarmProject.Domain.Common;
+using FarmProject.Domain.Constants;
+using FarmProject.Domain.Errors;
+
+namespace FarmProject.Domain.Models;
+
+public class Cage(string name) : Entity
+{
+    public string Name { get; private set; } = name;
+    public BreedingRabbit? MaleBreedingRabbit { get; private set; }
+    public BreedingRabbit? FemaleBreedingRabbit { get; private set; }
+    public int OffspringCount { get; private set; } = 0;
+    public OffspringType OffspringType { get; private set; } = OffspringType.Mixed;
+
+    public Result AssignBreedingRabbit(BreedingRabbit breedingRabbit)
+    {
+        if (breedingRabbit == null)
+            return Result.Failure(CageErrors.InvalidAssignment);
+
+        if (breedingRabbit.Gender == Gender.Female)
+            FemaleBreedingRabbit = breedingRabbit;
+        else
+            MaleBreedingRabbit = breedingRabbit;
+
+        return Result.Success();
+    }
+
+    public Result<BreedingRabbit> RemoveBreedingRabbit(Gender gender)
+    {
+        BreedingRabbit? rabbitToRemove;
+
+        if (gender == Gender.Female)
+        {
+            if (FemaleBreedingRabbit == null)
+                return Result.Failure<BreedingRabbit>(CageErrors.NoFemaleRabbit);
+
+            rabbitToRemove = FemaleBreedingRabbit;
+            FemaleBreedingRabbit = null;
+        }
+        else
+        {
+            if (MaleBreedingRabbit == null)
+                return Result.Failure<BreedingRabbit>(CageErrors.NoMaleRabbit);
+
+            rabbitToRemove = MaleBreedingRabbit;
+            MaleBreedingRabbit = null;
+        }
+
+        return Result.Success(rabbitToRemove);
+    }
+
+    public Result AddOffspring(int count)
+    {
+        if (count < 0)
+            return Result.Failure(CageErrors.NegativeOffspringNum);
+
+        OffspringCount += count;
+        return Result.Success();
+    }
+
+    public Result RemoveOffspring(int count)
+    {
+        if (count < 0)
+            return Result.Failure(CageErrors.NegativeOffspringNum);
+        if (count > OffspringCount)
+            return Result.Failure(CageErrors.OverOffspringNum);
+
+        OffspringCount -= count;
+        return Result.Success();
+    }
+
+    public void SetOffspringType(OffspringType offspringType)
+        => OffspringType = offspringType;
+}
