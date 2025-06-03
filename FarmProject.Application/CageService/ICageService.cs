52abc8b6e7db74dc158abae0ff93c2150e94bbad
MaxNoragami <maxcraftmd@gmail.com>
2025-06-03 16:08:57 +0300

    feat: add the declaration of the interfaces for both the cage service and repository

---
+++ FarmProject.Application/CageService/ICageService.cs
@@ -0,0 +1,19 @@
+﻿using FarmProject.Domain.Common;
+using FarmProject.Domain.Constants;
+using FarmProject.Domain.Models;
+
+namespace FarmProject.Application.CageService;
+
+public interface ICageService
+{
+    public Task<Result<Cage>> CreateCage(string name);
+    public Task<Result<List<Cage>>> GetAllCages();
+    public Task<Result<List<Cage>>> GetCagesWithAvailableSpace(Gender gender);
+    public Task<Result<Cage>> GetCageById(int cageId);
+    public Task<Result<Cage>> AssignBreedingRabbitToCage(int cageId, int breedingRabbitId);
+    public Task<Result<Cage>> RemoveBreedingRabbitFromCage(int cageId, Gender gender);
+    public Task<Result<Cage>> AddOffspringsToCage (int cageId, int count);
+    public Task<Result<Cage>> RemoveOffspringsFromCage(int cageId, int count);
+    public Task<Result<Cage>> UpdateOffspringType(int cageId, OffspringType offspringType);
+    
+}
