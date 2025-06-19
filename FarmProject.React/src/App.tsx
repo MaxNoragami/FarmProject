0374963351d18c856a7eaea4934ce48e310e06e9
MaxNoragami <maxcraftmd@gmail.com>
2025-06-20 01:13:27 +0300

    chore: init the react project

---
+++ FarmProject.React/src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+    const title = "App Component";
+    const views = 20;
+    const numUsers = [1, 2, 3, 4, 5]
+    const user = { name: "John", surname: "Pork", age: 32 }
+
+    return (
+        <div>
+            <div>
+                { title }
+            </div>
+            <div>
+                This has { views } views, I assume.
+            </div>
+            <div>
+                {numUsers }
+            </div>
+            <div>
+                Current user is { user.name + " " +  user.surname };
+            </div>
+        </div>
+    )
+}
+
+// To be able to use is somewhere else
+export default App
